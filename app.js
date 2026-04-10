fec665a4196ec2c58d4edcf430e03653c20c01f8
unknown <Admin@AID-5.CSE.COM>
2026-04-10 10:30:46 +0530

    app.js is changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app=express();
 const port = process.env.port ||3000 ;
 app.get("/",(req,res)=>{
-    res.send("welcome to shoeasy!");
+    res.send("welcome to shoeasy! from nivitha");
 })
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
